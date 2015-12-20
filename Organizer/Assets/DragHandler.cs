961a365d4e0304b7a98d42702adc82733c26d962
You Wu <urianawu@tamu.edu>
2015-12-20 11:10:12 +0800

    panel set up
    draggable slots implemented

---
+++ Organizer/Assets/DragHandler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public bool isDraggable = true;
+
+	public static GameObject itemBeingDragged;
+	Vector3 startPos;
+
+	#region IBeginDragHandler implementation
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		itemBeingDragged = gameObject;
+		startPos = transform.position;
+	}
+
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		if (isDraggable)
+		transform.position = Input.mousePosition; 
+
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		itemBeingDragged = null;
+		transform.position = startPos;
+	}
+
+	#endregion
+
+
+
+}
