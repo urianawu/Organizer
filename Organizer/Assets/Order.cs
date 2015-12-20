477a91bf63fbd40d6a2a51e83abf64b76780b3f6
You Wu <urianawu@tamu.edu>
2015-12-20 23:04:33 +0800

    check order function done

--- Organizer/Assets/Order.cs
+++ Organizer/Assets/Order.cs
@@ -5,11 +5,18 @@ using UnityEngine.UI;
 
 public class Order : MonoBehaviour, IHasChanged
 {
+	public GameObject start, end;
 	[SerializeField]
 	Transform slots;
+
+	Color startCol, endCol, diffCol;
 	// Use this for initialization
 	void Start ()
 	{
+		startCol = start.GetComponent<Image> ().color;
+		endCol = end.GetComponent<Image> ().color;
+		diffCol = endCol - startCol;
+
 		HasChanged ();
 	}
 	
@@ -17,17 +24,51 @@ public class Order : MonoBehaviour, IHasChanged
 	#region IHasChanged implementation
 	public void HasChanged ()
 	{
-		string printInfo = "";
-		foreach (Transform slotTransform in slots) {
+		bool inOrder = true;
+		Color prevColor = startCol;
+		Color curColor;
+		foreach(Transform slotTransform in slots) {
 			GameObject item = slotTransform.GetComponent<SlotHandler> ().item;
-			if (item) {
-				printInfo += item.GetComponent<Image>().color;
-				printInfo += " - ";
+			if (!item){
+				inOrder = false;
+				break;
+			}else {
+				curColor = item.GetComponent<Image>().color;
+				if (curColor != prevColor){
+					//check color order
+					if (!checkOrder(curColor, prevColor)){
+						print ("not in order");
+						inOrder = false;
+						break;
+					}else {
+						prevColor = curColor;
+					}
+				}
 			}
 		}
-		print (printInfo);
+		if (inOrder) {
+			print ("Organized!");
+		}
 	}
 	#endregion
+
+	bool checkOrder(Color curColor, Color prevColor) {
+		if (sameSign ((curColor - prevColor).r, diffCol.r) 
+			&& sameSign ((curColor - prevColor).g, diffCol.g)
+			&& sameSign ((curColor - prevColor).b, diffCol.b))
+			return true;
+		else 
+			return false;
+	}
+
+	bool sameSign(float a, float b){
+		if (a >= 0 && b >= 0)
+			return true;
+		else if (a <= 0 && b <= 0)
+			return true;
+		else
+			return false;
+	}
 }
 
 namespace UnityEngine.EventSystems
